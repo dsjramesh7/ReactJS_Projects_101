f3ddf11db080bb99d85d0e998a016c4781052a48
dsjramesh7 <rameshdussa07@gmail.com>
2025-01-20 15:48:49 +0530

    practice

--- movie-app/src/components/Navbar.jsx
+++ movie-app/src/components/Navbar.jsx
@@ -2,12 +2,18 @@ import Button from "./sharedUI/Button";
 
 const Navbar = () => {
   return (
-    <div className="shadow-xl w-full">
+    <div className="shadow-md bg-gray-700 text-white w-full h-20 flex items-center">
       <div className="container mx-auto">
-        <div className="flex justify-between p-4">
-          <h1>MovieMania</h1>
-          <div className="flex gap-4">
-            <input type="text" className="border border-red-500 p-2 w-96" />
+        <div className="flex justify-between items-center ">
+          <h1 className="text-2xl font-bold text-blue-400">
+            Movie<span className="text-red-400">Mania</span>
+          </h1>
+          <div className="hidden md:flex items-center gap-4 w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Search movies..."
+              className="w-full px-4 py-2 rounded-md bg-gray-700 border border-gray-600 focus:ring-2 focus:ring-blue-400 text-white"
+            />
             <Button variant="outline" color={"primary"} btnName={"Search"} />
           </div>
           <Button variant="primary" color="warn" btnName={"Sign Out"} />
