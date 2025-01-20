622ce4bb8e0d4fbe44d86318a3e4454503128026
dsjramesh7 <rameshdussa07@gmail.com>
2025-01-21 00:26:38 +0530

    practice

--- movie-app/src/components/MainSection.jsx
+++ movie-app/src/components/MainSection.jsx
@@ -1,43 +1,40 @@
-// import { useEffect, useState } from "react";
-// import { MOVIE_API_KEY } from "../utils/Constants";
+import { useEffect, useState } from "react";
+import { MOVIE_API_KEY } from "../utils/Constants";
 import MovieCard from "./MovieCard";
 
 const MainSection = () => {
-  // const [movieData, setMovieData] = useState(null);
+  const [movieData, setMovieData] = useState(null);
 
-  // const fetchingMovieData = async () => {
-  //   const response = await fetch(
-  //     `http://www.omdbapi.com/?s=Batman&apikey=${MOVIE_API_KEY}`
-  //   );
-  //   // console.log(response);
-  //   const data = await response.json();
-  //   setMovieData(data.Search);
-  // };
+  const fetchingMovieData = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=Spider Man&apikey=${MOVIE_API_KEY}`
+    );
+    // console.log(response);
+    const data = await response.json();
+    setMovieData(data.Search);
+  };
 
-  // useEffect(() => {
-  //   // fetchingMovieData();
-  // }, []);
+  useEffect(() => {
+    fetchingMovieData();
+  }, []);
 
-  // console.log(movieData);
+  console.log(movieData);
   return (
-    <div className="container mx-auto">
-      <MovieCard />
+    <div className="container mx-auto flex justify-center gap-3 mt-4 flex-wrap ">
+      {movieData && movieData.length > 0 ? (
+        movieData.map((m, index) => (
+          <MovieCard
+            key={index}
+            Title={m.Title}
+            Type={m.Type}
+            Year={m.Year}
+            Poster={m.Poster}
+          />
+        ))
+      ) : (
+        <h1>No Movie Found</h1>
+      )}
     </div>
-    // <div>
-    //   {movieData && movieData.length > 0 ? (
-    //     <>
-    //       {movieData.map((movie, index) => {
-    //         return (
-    //           <div className="text-red-500" key={index}>
-    //             {movie.Title}
-    //           </div>
-    //         );
-    //       })}
-    //     </>
-    //   ) : (
-    //     <h1>No Movie Found</h1>
-    //   )}
-    // </div>
   );
 };
 
--- movie-app/src/components/MovieCard.jsx
+++ movie-app/src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
-const MovieCard = () => {
+const MovieCard = ({ Title, Year, Poster, Type }) => {
   return (
     <div className="w-72 bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-2xl transition-shadow duration-300">
       <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhuK2yF9SqI7kmpz-lEct85j_mNWw68SjkMQ&s"
-        alt=""
+        src={Poster}
+        alt="movie-image"
+        className="w-full h-56 object-cover"
       />
-      <h1>ashdflsadfl lkasdfkl</h1>
-      <p>movie</p>
-      <p>1.2.34</p>
+      <div className="p-4">
+        <h1 className="text-lg font-semibold text-gray-800 truncate">
+          {Title}
+        </h1>
+        <p className="text-sm text-gray-500 mt-1">Type: {Type}</p>
+        <p className="text-sm text-gray-500 mt-1">Release Year:{Year}</p>
+        <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-all">
+          View Details
+        </button>
+      </div>
     </div>
   );
 };
