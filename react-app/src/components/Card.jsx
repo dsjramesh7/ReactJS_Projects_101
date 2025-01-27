774fec2b7f42d1fee6997cb00da7862dc4286bb9
dsjramesh7 <rameshdussa07@gmail.com>
2025-01-28 00:20:22 +0530

    useEffect

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Card = ({ title }) => {
   const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${isLiked}`);
+  });
   return (
     <div>
       <h1>{title}</h1>
