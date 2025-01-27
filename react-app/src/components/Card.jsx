92cdbc82fe06ecc9cbabbbe9e12d2662b0c76fec
dsjramesh7 <rameshdussa07@gmail.com>
2025-01-28 00:35:46 +0530

    useEffect

--- react-app/src/components/Card.jsx
+++ react-app/src/components/Card.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [isLiked, setIsLiked] = useState(false);
 
   useEffect(() => {
     console.log(`${title} has been liked: ${isLiked}`);
-  });
+  }, [isLiked]);
   return (
-    <div>
+    <div onClick={() => setCount(count + 1)}>
       <h1>{title}</h1>
+      <h3>{count}</h3>
       <button onClick={() => setIsLiked(!isLiked)}>
         {isLiked ? "💜" : "🤍"}
       </button>
