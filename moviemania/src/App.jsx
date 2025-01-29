48e5f9541dea873da4564fe05721368c958dc746
dsjramesh7 <rameshdussa07@gmail.com>
2025-01-30 00:14:00 +0530

    practice

--- moviemania/src/App.jsx
+++ moviemania/src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("I am batman");
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="hero.png" alt="movie-banner" />
           <h1>
             Find <span className="text-gradient">Movie</span> You'll Enjoy
             Without the Hassle
           </h1>
         </header>
+
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
