947a78d65b997df18893caa02e164d365c83060a
dsjramesh7 <rameshdussa07@gmail.com>
2025-02-01 16:38:48 +0530

    practice

--- moviemania/src/App.jsx
+++ moviemania/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import SearchBar from "./components/SearchBar";
 import { API_BASE_URL } from "../utils/Constants";
+import Loader from "./components/Loader";
+import MovieCard from "./components/MovieCard";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
@@ -16,26 +18,32 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage("");
     try {
-      const response = await fetch(
-        `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`,
-        API_OPTIONS
-      );
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       const data = await response.json();
+      console.log(data.results);
       setMovies(data.results);
     } catch (error) {
       setErrorMessage(`Failed to fetch movies: ${error.message}`);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main className="container mx-auto p-4">
@@ -48,17 +56,14 @@ const App = () => {
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </header>
 
-      {errorMessage ? (
+      {isLoading ? (
+        <Loader />
+      ) : errorMessage ? (
         <p className="text-red-500 text-center mt-4">{errorMessage}</p>
       ) : movies.length > 0 ? (
         <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
           {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="bg-gray-800 p-4 rounded-lg shadow-lg"
-            >
-              <h3 className="text-white font-semibold">{movie.title}</h3>
-            </div>
+            <MovieCard key={movie.id} movie={movie} />
           ))}
         </section>
       ) : (
